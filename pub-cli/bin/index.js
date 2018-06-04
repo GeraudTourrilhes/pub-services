0c9b2682ccc25d44be19528c422963a74d321aa8
Geraud Tourrilhes <geraud.tourrilhes@gmail.com>
2018-06-04 15:39:30 +0200

    pub-cli

--- pub-cli/bin/index.js
+++ pub-cli/bin/index.js
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
 var program = require('commander');
-program.version('1.0.0').option('-l, --list','Get the pubs list')
-    .parse(process.argv);
+program.version('1.0.0')
+.option('-l, --list','Get the pubs list')
+.option('-o, --opened','Get the pubs list Open')
+.parse(process.argv);
+
+//program.version('1.0.0').option('-o, --opened','Get the pubs list Open').parse(process.argv);
 if(program.list) {
     require('../src/main').getListPub();
+}
+
+
+if(program.opened) {
+    require('../src/main').getListPubOpen();
 }
